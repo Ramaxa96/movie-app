b0eb7f8fa11852bd0792cd284b43c97bf556645a
akvilo <oakvilo@gmail.com>
2025-10-28 21:50:27 +0200

    C
    Chore: minor changes in the structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
+import './App.scss'
+
+import Header from "./styles/components/Header/Header"
+import SectionHero from './styles/components/SectionHero/SectionHero'
+
 const App = () => {
   return (
     <>
+      <Header />  
+      <SectionHero />
     </>
   )
 }
