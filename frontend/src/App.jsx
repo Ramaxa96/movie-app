05f33d3bbf77308dc5ca36f78bad5f6c1963c586
akvilo <oakvilo@gmail.com>
2025-11-07 19:04:27 +0200

    build: minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './App.scss'
 import SectionRecommendation from './components/SectionRecommendation/SectionRecommendation'
 import Header from "./components/Header/Header"
 import SectionHero from './components/SectionHero/SectionHero'
+import SectionSelection from './components/SectionSelection/SectionSelection'
 
 const App = () => {
   return (
     <>
       <Header />  
       <SectionHero />
+      <SectionSelection />
       <SectionRecommendation />
     </>
   )
