c4bd5100625e2a43070acd051cb4f26741eeb92f
akvilo <oakvilo@gmail.com>
2025-11-07 19:07:03 +0200

    chore: reworking section recommendation

--- frontend/src/components/SectionRecommendation/SliderItem.jsx
+++ frontend/src/components/SectionRecommendation/SliderItem.jsx
@@ -3,25 +3,27 @@ import './SectionRecommendation.scss'
 import imageTest from '../../../src/assets/image/test-image.png'
 import Button from "../Button/Button"
 
-const SliderItem = () => {
+const SliderItem = (props) => {
+    const {
+        rating,
+        image,
+        title,
+        description
+    } = props
+
     return (
         <div className="section-recommend__item">
-            <div className="section-recommend__rating"></div>
-            <div className="section-recommend__poster">
+            <div className="section-recommend__item-poster">
+                <div className="section-recommend__item-rating">{rating}</div>
                 <img 
-                className='section-recommend__poster-image'
+                className='section-recommend__item-image'
                 src={imageTest} 
                 alt="film poster" 
                 />
             </div>
-            <div className="section-recommend__info">
-                <div className="section-recommend__details">
-                    <div className="section-recommend__details-title">Груз 200</div>
-                    <div className="section-recommend__details-desc">Комедия, 2007</div>
-                </div>
-                <div className="section-recommend__interaction">
-                    <Button size="md" color="gradient-blue" className="button section-recommend__interaction-button">Подробнее</Button>
-                </div>
+            <div className="section-recommend__item-info">
+                <div className="section-recommend__item-title">{title}</div>
+                <div className="section-recommend__item-desc">{description}</div>
             </div>
         </div>
     )
