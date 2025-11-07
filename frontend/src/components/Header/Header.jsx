9ee1f189afbf06960b87c40c9d96f5d824deb580
akvilo <oakvilo@gmail.com>
2025-11-07 19:06:26 +0200

    chore: minore changes in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,9 +7,15 @@ const Header = () => {
             <a className='header__title' href="/">Moodi</a>
             <nav className="header__menu">
                 <ul className="header__menu-list">
-                    <li className="header__menu_item"><a href='/'>Рекомендации дня</a></li>
-                    <li className="header__menu_item"><a href='/'>Популярное сейчас</a></li>
-                    <li className="header__menu_item"><a href='/'>Подборки</a></li>
+                    <li className="header__menu-item">
+                        <a href='/'>Рекомендации дня</a>
+                    </li>
+                    <li className="header__menu-item">
+                        <a href='/'>Популярное сейчас</a>
+                    </li>
+                    <li className="header__menu-item">
+                        <a href='/'>Подборки</a>
+                    </li>
                 </ul>
             </nav>
             <div className="header__menu-actions">
